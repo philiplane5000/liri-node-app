69fa32642473b99f4e229e9d0fbd5d9dc7f2c490
philtd5000 <philiptaft9@gmail.com>
2018-07-17 12:32:29 -0700

    difficult with spotify request

--- liri.js
+++ liri.js
@@ -1,15 +1,19 @@
 const result = require("dotenv").config();
 const keys = require("./keys");
 const Twitter = require('twitter');
-var Spotify = require('spotify');
+const Spotify = require('node-spotify-api');
 
+//AUTHENTICATE USER:
 // let spot = new Spotify(keys.spotify);
 let client = new Twitter(keys.twitter);
+let spotify = new Spotify(keys.spotify);
 
+//USER INPUT:
 let command = process.argv[2];
 let input = process.argv[3];
 
-
+// MAIN SWITCH STATEMENT:    //
+// ************************* //
 switch (command) {
     case 'my-tweets':
         //TWITTER GET PATH:
@@ -23,8 +27,21 @@ switch (command) {
         console.log('UNDER CONSTRUCTION');
         break;
     default:
-        console.log(`Sorry, unable to process: '${command}'`);
+        console.log('DEFAULT ACTION');
+        // console.log(`Sorry, unable to process: '${command}'`);
 }
+
+// SANDBOX:
+// *********** //
+
+spotify
+  .search({ type: 'track', query: 'All the Small Things' })
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(err) {
+    console.log('ERROR');
+  });
 
 // ALL FUNCTIONS:
 // ************* //
