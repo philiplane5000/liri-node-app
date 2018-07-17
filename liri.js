055b321c44258953b2983ab7bf9abeafa9c3ce0e
philtd5000 <philiptaft9@gmail.com>
2018-07-17 15:16:43 -0700

    cleaner switch statement including callback functions

--- liri.js
+++ liri.js
@@ -1,58 +1,47 @@
-const result = require("dotenv").config();
-const keys = require("./keys");
+const result  = require("dotenv").config();
+const keys    = require("./keys");
 const Twitter = require('twitter');
 const Spotify = require('node-spotify-api');
 
 //AUTHENTICATE USER:
 // let spot = new Spotify(keys.spotify);
-let client = new Twitter(keys.twitter);
+let client  = new Twitter(keys.twitter);
 let spotify = new Spotify(keys.spotify);
 
 //USER INPUT:
 let command = process.argv[2];
-let input = process.argv[3];
+let input   = process.argv[3];
 
-// MAIN SWITCH STATEMENT:    //
+//HARD-CODED (MAYBE LATER USE THE INQUIRER THING TO GATHER AND SET USER SCREEN NAME?)
+let twitterScreenName = 'MrPotat58309442';
+
+// MAIN SWITCH STATEMENT (SWITCH DETERMINED BY COMMAND INPUT):    //
 // ************************* //
 switch (command) {
     case 'my-tweets':
-        //TWITTER GET PATH:
-        // client.get(path, params, callback);
-        client.get('statuses/user_timeline', { screen_name: 'MrPotat58309442' }, twitterLog);
+        twitterGet(twitterScreenName, twitterLog);
         break;
     case 'spotify-this-song':
         spotifyThis(input);
-    break;
+        break;
     case 'movie-this':
         console.log('UNDER CONSTRUCTION');
         break;
     default:
-        console.log('DEFAULT ACTION');
-    // console.log(`Sorry, unable to process: '${command}'`);
+        console.log(`Sorry, unable to process: '${command}'`);
 }
 
 // SANDBOX:
 // *********** //
 
-function spotifyThis(trackName) {
-    
-    spotify.search({ type: 'track', query: trackName, limit: 1 }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        
-        console.log(`ARTIST NAME:\n"${data.tracks.items[0].artists[0].name}" `);
-        console.log(`SONG NAME:\n"${data.tracks.items[0].name}"`);
-        console.log(`ALBUM:\n"${data.tracks.items[0].album.name}"`);
-        console.log(`PREVIEW LINK:\n"${data.tracks.items[0].external_urls.spotify}"`)
-        
-    });
-}
-
-
 
 // ALL FUNCTIONS: //
 // ************** //
+
+//TWITTER FUNCTIONS:
+function twitterGet(screenName, callback) {
+    client.get('statuses/user_timeline', { screen_name: screenName }, callback);
+}
 
 function twitterLog(error, tweets, response) {
     if (!error) {
@@ -69,5 +58,20 @@ function twitterLog(error, tweets, response) {
 
 }
 
+//SPOTIFY FUNCTIONS:
+function spotifyThis(trackName) {
+
+    spotify.search({ type: 'track', query: trackName, limit: 1 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(`ARTIST NAME:\n"${data.tracks.items[0].artists[0].name}" `);
+        console.log(`SONG NAME:\n"${data.tracks.items[0].name}"`);
+        console.log(`ALBUM:\n"${data.tracks.items[0].album.name}"`);
+        console.log(`PREVIEW LINK:\n"${data.tracks.items[0].external_urls.spotify}"`)
+
+    });
+}
 
 
