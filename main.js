82cb9515ee3a3ac702ec23209afadf6733adf676
philtd5000 <philiptaft9@gmail.com>
2018-07-20 11:45:26 -0700

    basic inquirer functionality

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
 const app = require('./liri');
+const inquirer = require('inquirer');
 
 //CHECK LIRI CONNECTION:
 app.liri.connected();
 
-//USER INPUT:
-let command = process.argv[2];
-let input   = process.argv[3];
+runLiri();
 
-//PREVENT APPEND LOG OF 'UNDEFINED' IN THE CASE OF NO INPUT E.G. 'MY-TWEETS':
-(input === undefined) ? app.liri.appendLog(command) : app.liri.appendLog(command, input)
+function runLiri() {
 
-//RUN LIRI:
-app.liri.mainSwitch(command, input)
+    inquirer.prompt([
+        {
+            message: "Select a LIRI command:",
+            type: "checkbox",
+            name: "command",
+            choices: ['my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says']
+        },{
+            name: "twitterScreenName",
+            message: "What is your Twitter Screen Name?"
+        }, {
+            name: "input",
+            message: "Enter Liri Input:"
+        }
+    ]).then(function (answers) {
+        //USER INPUT:
+        app.liri.twitterScreenName = answers.twitterScreenName;
+        let command = answers.command[0];
+        let input = answers.input;
+
+        //PREVENT APPEND LOG OF 'UNDEFINED' IN THE CASE OF NO INPUT E.G. 'MY-TWEETS':
+        (input === undefined) ? app.liri.appendLog(command) : app.liri.appendLog(command, input)
+        //RUN LIRI USING INQUIRER INPUTS:
+        app.liri.mainSwitch(command, input)
+
+    });
+
+}
+
+
+
 
